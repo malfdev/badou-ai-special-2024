62ffcbfedc295fa301e03d71efb8bd9974d7a595
malf <cutemalinfei@outlook.com>
2024-07-15 10:28:15 +0800

    fashion_mnist_keras accuracy display

--- 52-马林飞/第九周/fashion_mnist_keras.py
+++ 52-马林飞/第九周/fashion_mnist_keras.py
@@ -30,11 +30,39 @@ test_labels = to_categorical(test_labels)
 network = models.Sequential()  # 模型初始化
 network.add(layers.Flatten(input_shape=(28, 28)))
 network.add(layers.Dense(units=512, activation='relu', input_shape=(28, 28, 1)))
+network.add(layers.Dropout(rate=0.5))
 network.add(layers.Dense(units=10, activation="softmax"))
 network.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # 训练
-history = network.fit(train_images, train_labels, epochs=5, batch_size=128, verbose=1)
+history = network.fit(train_images, train_labels, epochs=5, batch_size=128, verbose=1,
+                      validation_data=(test_images, test_labels))
 
 # 测试
 test_loss, test_acc = network.evaluate(test_images, test_labels, verbose=1)
+
+epochs = range(1, len(history.history['loss']) + 1)
+
+print(history.history.keys())
+
+plt.figure(figsize=(12, 4))
+
+# 绘制训练和验证的损失值
+plt.subplot(1, 2, 1)
+plt.plot(epochs, history.history['loss'], 'r', label='Training loss')
+plt.plot(epochs, history.history['val_loss'], 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+# 绘制训练和验证的准确率
+plt.subplot(1, 2, 2)
+plt.plot(epochs, history.history['acc'], 'bo', label='Training accuracy')
+plt.plot(epochs, history.history['val_acc'], 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
